e7c676da4ca1c7f9cbc2d6dbbb10cfdf6ae87f2a
George <enageo20@gmail.com>
2025-11-14 13:49:37 +0200

    Update config_flow.py

--- custom_components/cmetb/config_flow.py
+++ custom_components/cmetb/config_flow.py
@@ -1,8 +1,8 @@
 import voluptuous as vol
 from homeassistant import config_entries
-from homeassistant.core import callback
+from .const import DOMAIN, SECTORI_PUNCTE
 
-class CmtebConfigFlow(config_entries.ConfigFlow, domain="cmteb"):
+class CmtebConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
     """Handle a config flow for CMTEB."""
 
     VERSION = 1
@@ -12,16 +12,32 @@ class CmtebConfigFlow(config_entries.ConfigFlow, domain="cmteb"):
         errors = {}
 
         if user_input is not None:
+            # Salvează configurația
+            punct_complet = f"{user_input['sector']} - {user_input['nume_punct']}"
+            
             return self.async_create_entry(
-                title=user_input["nume_punct"],
+                title=punct_complet,
                 data=user_input
             )
 
+        # Creează schema dinamic cu sectoare și puncte
+        schema = vol.Schema({
+            vol.Required("sector"): vol.In({
+                "sector_1": "Sector 1",
+                "sector_2": "Sector 2", 
+                "sector_3": "Sector 3",
+                "sector_4": "Sector 4",
+                "sector_5": "Sector 5",
+                "sector_6": "Sector 6"
+            }),
+            vol.Required("nume_punct"): str
+        })
+
         return self.async_show_form(
             step_id="user",
-            data_schema=vol.Schema({
-                vol.Required("nume_punct"): str,
-                vol.Required("id_punct"): str
-            }),
-            errors=errors
+            data_schema=schema,
+            errors=errors,
+            description_placeholders={
+                "mesaj": "Selectează sectorul și introdu numele punctului termic"
+            }
         )
