27a22d62a53cc0504dac12f0f9c89f9db7a22016
George <enageo20@gmail.com>
2025-11-15 22:00:02 +0200

    Update __init__.py

--- custom_components/cmetb/__init__.py
+++ custom_components/cmetb/__init__.py
@@ -1,13 +1,25 @@
 import logging
 from homeassistant.config_entries import ConfigEntry
 from homeassistant.core import HomeAssistant
+from .const import DOMAIN, CONF_SECTOR, CONF_NUME_PUNCT
 
 _LOGGER = logging.getLogger(__name__)
+PLATFORMS = ["sensor"]
 
 async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     """Set up CMTEB from a config entry."""
     hass.data.setdefault(DOMAIN, {})
     
+    # Store config
+    hass.data[DOMAIN][entry.entry_id] = entry.data
+    
     # Setup sensor platform
     await hass.config_entries.async_forward_entry_setups(entry, PLATFORMS)
     return True
+
+async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
+    """Unload a config entry."""
+    unload_ok = await hass.config_entries.async_unload_platforms(entry, PLATFORMS)
+    if unload_ok:
+        hass.data[DOMAIN].pop(entry.entry_id)
+    return unload_ok
