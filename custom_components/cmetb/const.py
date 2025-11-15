1bcd1eca92e8b4f246afa164f23445073e535359
George <enageo20@gmail.com>
2025-11-15 21:57:57 +0200

    Update const.py

--- custom_components/cmetb/const.py
+++ custom_components/cmetb/const.py
@@ -1,4 +1,19 @@
 """Constants for CMTEB integration."""
+
 DOMAIN = "cmteb"
-CONF_SECTOR = "sector"
-CONF_NUME_PUNCT = "nume_punct"
+NAME = "CMTEB Termoficare Bucuresti"
+VERSION = "1.0.0"
+ATTRIBUTION = "Date preluate de la CMTEB București"
+
+CONF_PUNCT_TERMIC = "punct_termic"
+
+STATUS_OPRIT = "Oprire"
+STATUS_NORMAL = "Functionare normala"
+STATUS_DEFECT = "Deficienta"
+
+AFECTAT_ACC = "Apa calda (ACC)"
+AFECTAT_INC = "Caldura (INC)"
+
+COLOR_ROSU = "#FF0000"
+COLOR_VERDE = "#00FF00"
+COLOR_GALBEN = "#FFFF00"
