0c90fc82375d05d701a48e26a3a820aa26507d95
George <enageo20@gmail.com>
2025-11-22 21:13:34 +0200

    Update __init__.py

--- custom_components/cmteb_termoficare/__init__.py
+++ custom_components/cmteb_termoficare/__init__.py
@@ -3,6 +3,8 @@ import logging
 from homeassistant.config_entries import ConfigEntry
 from homeassistant.core import HomeAssistant
 
+from .services import async_setup_services
+
 _LOGGER = logging.getLogger(__name__)
 
 DOMAIN = "cmteb_termoficare"
@@ -10,6 +12,9 @@ DOMAIN = "cmteb_termoficare"
 async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     """Set up cmteb_termoficare from a config entry."""
     hass.data.setdefault(DOMAIN, {})
+    
+    # Setup services
+    await async_setup_services(hass)
     
     # Forward the setup to the sensor platform
     await hass.config_entries.async_forward_entry_setups(entry, ["sensor"])
