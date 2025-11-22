44dda350275051c12e9ac99ef26ad20a01da3ea4
George <enageo20@gmail.com>
2025-11-22 16:32:17 +0200

    Update const.py

--- custom_components/cmteb_termoficare/const.py
+++ custom_components/cmteb_termoficare/const.py
@@ -1,3 +1,4 @@
-"""Constante pentru integrarea CMTEB Termoficare."""
+"""Constants for the CMTEB Termoficare integration."""
+
 DOMAIN = "cmteb_termoficare"
-URL_CMTEB = "https://www.cmtEB.ro/functionare_sistem_termoficare.php"
+URL_CMTEB = "https://www.cmteb.ro/functionare_sistem_termoficare.php"
