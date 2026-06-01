982b0afcd0e4e8ffd0649721d63dbf367d971a5e
George <241097702+GeorgeRPI@users.noreply.github.com>
2026-06-02 00:24:12 +0300

    Update __init__.py

--- custom_components/cmteb_termoficare/__init__.py
+++ custom_components/cmteb_termoficare/__init__.py
@@ -1,27 +1,12 @@
-"""The cmteb_termoficare integration."""
-import logging
+"""CMTEB Termoficare integration."""
 from homeassistant.config_entries import ConfigEntry
 from homeassistant.core import HomeAssistant
-
 from .const import DOMAIN
 
-_LOGGER = logging.getLogger(__name__)
-
-
 async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
-    """Set up cmteb_termoficare from a config entry."""
     hass.data.setdefault(DOMAIN, {})
-
-    if "services" not in hass.data[DOMAIN]:
-        from .services import async_setup_services
-        await async_setup_services(hass)
-        hass.data[DOMAIN]["services"] = True
-
     await hass.config_entries.async_forward_entry_setups(entry, ["sensor"])
     return True
 
-
 async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
-    """Unload a config entry."""
-    await hass.config_entries.async_forward_entry_unload(entry, "sensor")
-    return True
+    return await hass.config_entries.async_unload_platforms(entry, ["sensor"])
